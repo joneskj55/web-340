42c9b3f355af4344751f4f512f4a22c070985dd2
joneskj55 <joneskj55@gmail.com>
2021-01-07 14:04:37 -0600

    add 2.4

---
+++ week-2/ejs-views/jones-assignment-2.4.js
@@ -0,0 +1,36 @@
+/*
+==========================================
+; Title: Exercise 2.4 - EJS Views
+; Author: Kevin Jones
+; Date: 7 January 2021
+; Description: This exercise creates a Node
+; server and returns the index page.
+; ========================================
+*/
+
+const http = require("http"); // Require http
+const express = require("express"); // Require Express
+const path = require("path"); // Require path
+
+// Create/ initialize the Express app.
+const app = express();
+
+// Tell Express where the views are located.
+app.set("views", path.resolve(__dirname, "views"));
+
+// Tell Express to use the EJS view engine.
+app.set("view engine", "ejs");
+
+// Get the response and render it in the browser.
+app.get("/", function (request, response) {
+  response.render("index", {
+    firstName: "Kevin",
+    lastName: "Jones",
+    address: "123 Nunya Bizness Dr.",
+  });
+});
+
+// Create server and listen on port 8080.
+http.createServer(app).listen(8080, function () {
+  console.log("EJS-Views app started on port 8080.");
+});
