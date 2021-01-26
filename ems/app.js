eba98aef51858a8fe2d219a79a9b0738e30f551b
joneskj55 <joneskj55@gmail.com>
2021-01-26 15:25:16 -0600

    add comments

--- ems/app.js
+++ ems/app.js
@@ -8,25 +8,32 @@
 ; ========================================
 */
 
+// Import libraries
 const express = require("express");
 const http = require("http");
 const path = require("path");
 const logger = require("morgan");
 
+// Initialize app
 const app = express();
 
+// Tell Express where the views are
 app.set("views", path.resolve(__dirname, "views"));
 
+// Tell Express to use EJS
 app.set("view engine", "ejs");
 
+// Log with Morgan's short format
 app.use(logger("short"));
 
+// Handle the response to the index page
 app.get("/", function (request, response) {
   response.render("index", {
     title: "Home Page",
   });
 });
 
+// Create server and listen on port 3000
 http.createServer(app).listen(3000, function () {
   console.log("Application started on port 3000!");
 });
